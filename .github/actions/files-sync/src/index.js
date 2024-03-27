3d1a413ce93c1d6a8e2c450d423687a8965a38c1
Rocco Aliberti <raliberti84@gmail.com>
2024-03-27 10:48:09 +0100

    feat: added checks on repos ORG_*_SKIP variable prior to copy a file

--- .github/actions/files-sync/src/index.js
+++ .github/actions/files-sync/src/index.js
@@ -80,7 +80,7 @@ const updateProjectAutomationRepos = async () => {
             repoWorkflow = repoWorkflow.replace(/{{{ISSUE_MANAGE_PAT}}}/g, issueManagePat);
         }
 
-        await _createOrUpdateFile(
+        await _createOrUpdateFileContent(
             secrets[reposConfig[repo].secrets?.['workflow-manage'] ?? ''] ?? '',
             repo,
             '.github/workflows/project-automation.yml',
@@ -94,40 +94,22 @@ const updateProjectAutomationRepos = async () => {
  * Create, update or delete the PHPUnit automation workflow on each repository.
  */
 const updatePHPUnitAutomationRepos= async () => {
+    const
+        skipVar = 'ORG_PHPUNIT_SKIP',
+        filesToCheck = [ // To check files existence.
+            'phpunit.xml',
+            '.phpunit.xml.dist',
+            'phpunit.xml.dist',
+            'phpunit.ruleset.xml',
+        ];
 
-    // Check files existence
-    const filesToCheck = [
-        'phpcs.xml',
-        '.phpcs.xml.dist',
-        'phpcs.xml.dist',
-        'phpcs.ruleset.xml',
-    ];
-
-    // Read the template.
-    const workflow = readFileSync(
-        `${ process.env.GITHUB_WORKSPACE }/.github/workflow-templates/phpunit.yml`, 'utf8'
+    await _createOrUpdateFile(
+        `${ process.env.GITHUB_WORKSPACE }/.github/workflow-templates/phpunit.yml`,
+        '.github/workflows/phpunit.yml',
+        'workflow-manage',
+        skipVar,
+        filesToCheck
     );
-
-    // For each company's repository create, update or delete the phpunit workflow.
-    for ( const repo in reposConfig ) {
-        // Skip not owned repo.
-        if (! reposConfig[repo].owner && !processDeletion) {
-            continue;
-        }
-        if (await _checkRepoFilesExist(filesToCheck, repo)) {
-            await _createOrUpdateFile(
-                secrets[reposConfig[repo].secrets?.['workflow-manage'] ?? ''] ?? '',
-                repo,
-                '.github/workflows/phpunit.yml',
-                workflow
-            );
-        } else {
-            console.log(
-                'Skipping %s: The repository does not contain a phpunit config file',
-                repo
-            );
-        }
-    }
 };
 
 /**
@@ -135,38 +117,75 @@ const updatePHPUnitAutomationRepos= async () => {
  */
 const updatePHPCSAutomationRepos = async () => {
 
-    // Check files existence
-    const filesToCheck = [
-        'phpcs.xml',
-        'phpcs.xml.dist',
-        'phpcs.ruleset.xml',
-    ];
+    const
+        skipVar = 'ORG_PHPCS_SKIP',
+        filesToCheck = [ // To check files existence.
+            'phpcs.xml',
+            'phpcs.xml.dist',
+            'phpcs.ruleset.xml',
+        ];
 
-    // Read the template.
-    const workflow = readFileSync(
-        `${ process.env.GITHUB_WORKSPACE }/.github/workflow-templates/phpcs.yml`, 'utf8'
+    await _createOrUpdateFile(
+        `${ process.env.GITHUB_WORKSPACE }/.github/workflow-templates/phpcs.yml`,
+        '.github/workflows/phpcs.yml',
+        'workflow-manage',
+        skipVar,
+        filesToCheck
     );
+};
 
-    // For each company's repository create, update or delete the phpcs workflow.
-    for ( const repo in reposConfig ) {
-        // Skip not owned repo.
-        if (! reposConfig[repo].owner && !processDeletion) {
-            continue;
-        }
+/**
+ * Check if certain conditions are met to skip a repository.
+ *
+ * A repo is skipped if any of the following conditions are met:
+ * - the repository is not owned and we don't allow the file deletion;
+ * - we require to check a skipVar and the repo var is set to require the skip;
+ * - we require the existence of some files and they are not present.
+ *
+ * @param repo    The name of the repository.
+ * @param skipVar A repo variable name to check against to determine if the repo should be skipped.
+ * @param filesToCheck List of files to check for existence to determine if the repo should be skipped.
+ * @returns Whether to skip the repository or not.
+ */
+const _skipRepo = async (repo, skipVar, filesToCheck) => {
+    return (
+        // If the repository is not owned and we don't allow the file deletion.
+        ( ! reposConfig[repo].owner && !processDeletion ) ||
+        // If we require to check a skipVar and the repo var is set to require the skip.
+        ( skipVar && await _repoVarRequiresSkip(repo, skipVar) ) ||
+        // If we require the existence of some files and they are not present.
+        ( filesToCheck && ! await _checkRepoFilesExist(filesToCheck, repo) )
+    );
+};
 
-        if (await _checkRepoFilesExist(filesToCheck, repo)) {
-            await _createOrUpdateFile(
-                secrets[reposConfig[repo].secrets?.['workflow-manage'] ?? ''] ?? '',
+/**
+ * Check if a repository requires to be skipped based on a action variable.
+ *
+ * @param repo    The repository to check.
+ * @param skipVar The repository skip variable to check.
+ * @returns Returns true if the repo needs to be skipped, false otherwise.
+ */
+const _repoVarRequiresSkip = async (repo, skipVar) => {
+    const octokitVarRead = _getOctokitInstance(secrets.CSPF_REPO_VARS_READ_PAT);
+    try {
+        const {data} = await octokitVarRead.request(
+            'GET /repos/{org}/{repo}/actions/variables/{skipVar}',
+            {
+                org,
                 repo,
-                '.github/workflows/phpcs.yml',
-                workflow
-            );
-        } else {
-            console.log(
-                'Skipping %s: The repository does not contain a phpcs config file',
-                repo
-            );
+                skipVar
+            }
+        );
+        return 'true' === (data.value ?? false);
+    } catch (error) {
+        if (error.status === 403) {
+            // Authentication issue.
+            console.log(error);
+            core.setFailed(error.messages) ;
+            throw error;
         }
+        // If the variable is not found, we assume the automation is allowed (return false).
+        return 404 !== error.status;
     }
 };
 
@@ -178,7 +197,7 @@ const updatePHPCSAutomationRepos = async () => {
  * @returns Returns true if at least one file is found, false after checking all files.
  */
 const _checkRepoFilesExist = async (filesToCheck, repo) => {
-    const octokitRead =  _getOctokitInstance(secrets.CSPF_REPO_READ_PAT);
+    const octokitRead = _getOctokitInstance(secrets.CSPF_REPO_READ_PAT);
     for ( const path of filesToCheck ) {
         try {
             await octokitRead.request(
@@ -199,15 +218,55 @@ const _checkRepoFilesExist = async (filesToCheck, repo) => {
 };
 
 /**
+ * Update a generic file based on the source file, destination file, secret key, skip variable, and files to check.
+ *
+ * @param sourceFile The path to the source file.
+ * @param destFile The path to the destination file.
+ * @param secretKey The secret key, in the repo secrets array, to use to manage the file.
+ * @param skipVar A repo variable name to check against to determine if the repo should be skipped.
+ * @param filesToCheck List of files to check for existence to determine if the repo should be skipped.
+ */
+const _createOrUpdateFile = async (sourceFile, destFile, secretKey, skipVar, filesToCheck) => {
+    // Read the template.
+    const content = readFileSync(
+        sourceFile, 'utf8'
+    );
+
+    if (!content) {
+        return;
+    }
+    // For each company's repository create, update or delete file.
+    for ( const repo in reposConfig ) {
+        try {
+            if ( ! (await _skipRepo(repo, skipVar, filesToCheck)) ) {
+                await _createOrUpdateFileContent(
+                    secrets[reposConfig[repo].secrets?.[secretKey] ?? ''] ?? '',
+                    repo,
+                    destFile,
+                    content
+                );
+            } else {
+                console.log(
+                    'Skipping %s: The repository is not owned by any team, or misses the required files, or opted out via the %s variable',
+                    repo,
+                    skipVar
+                );
+            }
+        } catch(error) {
+            // Error already down the line.
+        }
+    }
+};
+
+/**
  * Create or update a file on a repository.
  *
- * @param secret string  The secret to use to create the file.
- * @param repo   string  The repo.
- * @param file   string  The file to create or update (or delete).
- * @param content string The content of the file.
+ * @param secret The secret to use to create the file.
+ * @param repo The repo.
+ * @param file The file to create or update (or delete).
+ * @param content The content of the file.
  */
-const _createOrUpdateFile = async (secret, repo, file, content) => {
-
+const _createOrUpdateFileContent = async (secret, repo, file, content) => {
     if (! content && !processDeletion) {
         console.log(
             'Skipping %s: The repository is not associated to any teams, and workflow deletion is disabled: see process_deletion action\'s parameter.',
@@ -245,8 +304,8 @@ const _createOrUpdateFile = async (secret, repo, file, content) => {
 /**
  * Retrieves Octokit instance: if not already cached, creates and caches it.
  *
- * @param key  string Octokit instance key in the cache, usually a token.
- * @param type string Can be 'global' or 'textCRUD'. Default is 'global'.
+ * @param key Octokit instance key in the cache, usually a token.
+ * @param type Can be 'global' or 'textCRUD'. Default is 'global'.
  * @returns Octokit instance or Octokit instance with the plugin `createOrUpdateTextFile`, associated with the given key.
  */
 const _getOctokitInstance = (key, type) => {
@@ -276,7 +335,6 @@ const _getOctokitInstance = (key, type) => {
  * Main.
  */
 const main = async () => {
-
     switch (processDeletion){
     case 'true':
     case true:
@@ -300,7 +358,6 @@ const main = async () => {
         await updatePHPUnitAutomationRepos();
         break;
     }
-
 };
 
 main().catch( err => core.setFailed( err.message ) );
